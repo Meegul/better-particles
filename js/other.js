8c425d413b358aa4d6798a2fd3038d5c1587d1a3
meegul304 <micha3lbilstein@gmail.com>
2017-01-19 19:08:14 -0500

    Renamed and removed unfinished demo

--- js/other.js
+++
@@ -1,7 +0,0 @@
-let particleObject;
-function init() {
-    particleObject = new Particles(Particles.configs().snow, document.getElementById("particles"));
-    particleObject.start();
-}
-
-window.onload = init;
