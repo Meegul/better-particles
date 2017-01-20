b4aabb5e563ef094cb5cee3af91de114f8e2e43c
meegul304 <micha3lbilstein@gmail.com>
2017-01-19 19:50:21 -0500

    Added license to file

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const PORT = 8080;
-
-app.use(express.static('./'));
-
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-});
